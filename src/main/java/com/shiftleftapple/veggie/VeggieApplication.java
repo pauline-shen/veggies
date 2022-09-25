b4e13be9d9d86b33bffbf2071d7f4ea1e73886e4
pauline-shen <s9633291@gmail.com>
2022-09-25 16:08:15 -0400

    init push

---
+++ src/main/java/com/shiftleftapple/veggie/VeggieApplication.java
@@ -0,0 +1,13 @@
+package com.shiftleftapple.veggie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeggieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeggieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shiftleftapple/veggie/VeggieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shiftleftapple.veggie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeggieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
